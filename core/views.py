f3d0765838e692aa653c5f842228aa3b79603d40
Kushal KC <kushalkc@Kushals-Air.broadband>
2026-06-15 00:25:37 +0100

    select and prefetch queries

--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
+from django.db.models import Prefetch, Sum
 from django.shortcuts import render
 
+from core.models import Restaurant, Rating, Sale
+from django.utils import timezone
+
 # Create your views here.
+def index(request):
+    #Get all 5 star ratings, fetch all the sales for restaurant with 5-star rating
+    month_ago = timezone.now() - timezone.timedelta(days=30)
+    monthly_sales = Prefetch(
+        'sales',
+        queryset=Sale.objects.filter(datetime__gt=month_ago)
+    )
+    # restaurants = Restaurant.objects.prefetch_related('ratings', 'sales') \
+    #     .filter(ratings__rating=5) \
+    #         .annotate(total=Sum('sales__income'))
+
+    restaurants = Restaurant.objects.prefetch_related('ratings', monthly_sales) \
+        .filter(ratings__rating=5) 
+    restaurants = restaurants.annotate(total=Sum('sales__income'))
+
+    # restaurants = Restaurant.objects.filter(name__istartswith='c').prefetch_related('ratings', 'sales')
+    ratings = Rating.objects.only('rating', 'restaurant__name').select_related('restaurant')
+    context = {"restaurants":restaurants, "ratings":ratings}
+    print([r.total for r in restaurants])
+    return render(request, 'index.html', context)
--- orm_series/settings.py
+++ orm_series/settings.py
@@ -26,13 +26,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'core'
+    'core',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -105,3 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
